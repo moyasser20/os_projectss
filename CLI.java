683c56ba73f5de6917c39c9259804361da3a6654
moyasser20 <mohamedyasser192023@gmail.com>
2024-10-28 13:51:32 +0300

    new

--- CLI.java
+++ CLI.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class CLI {
+import java.io.File; // to manipulate files and directories in the filesystem.
+
+public class CLI {
+    private File currentDirectory;
+
+    public CLI()
+    {
+        this.currentDirectory = new File(System.getProperty("user.dir"));
+    }
+
+    public String pwd()
+    {
+        return  currentDirectory.getAbsolutePath();
+    }
+
+
+
 }
--- CLITest.java
+++ CLITest.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class CLITest {
+import org.junit.Before;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+
+
+public class CLITest {
+    private  CLI cli;
+
+    @BeforeEach
+    public void setUp()
+    {
+        cli = new CLI();
+    }
+
+    @Test
+    public void testPwd()
+    {
+        assertEquals(System.getProperty("user.dir"), cli.pwd());
+    }
+
 }
